1df88f844bb1e8f447980c8357d762dcc37ea488
samadrita2145 <86518116+samadrita2145@users.noreply.github.com>
2021-07-11 20:39:20 +0530

    Add files via upload

---
+++ AngryBirdsStage5-master/AngryBirdsStage5-master/Box.js
@@ -0,0 +1,20 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height){
+    super(x,y,width,height);
+    this.image = loadImage("sprites/wood1.png");
+    this.Visibility=255;
+  }
+display(){
+  if(this.body.speed<3){
+    super.display();
+  }
+  else{
+    World .remove(world, this.body);
+    push ()
+    this.Visibility-=5;
+    tint (255, this.Visibility);
+    image (this.image, this.body.position.x, this.body.position.y, 70, 70);
+    pop();
+  }
+}
+};
